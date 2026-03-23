ab78cafcaf915c39f994a94ca09a0c0847d07a74
Juan villada <villadaurechejuandavid20@gmail.com>
2026-03-22 22:42:05 -0500

    agregar PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'FinanzApp',
+        short_name: 'FinanzApp',
+        description: 'Controla tus finanzas personales',
+        theme_color: '#5b5fcf',
+        background_color: '#0a0a0d',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
 })
